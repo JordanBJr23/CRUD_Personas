db8902bcc6d975a178b015a5409f408a5793b3d2
JordanBjr23 <jordanmarz12@gmail.com>
2023-04-15 23:29:13 -0600

    Terminado

--- src/Vistas/Registro.java
+++ src/Vistas/Registro.java
@@ -16,44 +16,54 @@ public class Registro extends javax.swing.JFrame {
         this.setLocationRelativeTo(null);
         this.setResizable(false);
         this.setTitle("CRUD_Personas");
+        this.jtxtVerDatos.setEditable(false);
+        actualizarTable();
     }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        btnGuardar = new javax.swing.JButton();
+        jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
+        txtNombre = new javax.swing.JTextField();
         jLabel2 = new javax.swing.JLabel();
+        txtTelefono = new javax.swing.JTextField();
         jLabel3 = new javax.swing.JLabel();
         txtGmail = new javax.swing.JTextField();
-        txtNombre = new javax.swing.JTextField();
-        txtTelefono = new javax.swing.JTextField();
         btnLimpiar = new javax.swing.JButton();
-        btnConsultar = new javax.swing.JButton();
+        btnGuardar = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         jtxtVerDatos = new javax.swing.JTextArea();
+        btnEliminar = new javax.swing.JButton();
+        btnModificar = new javax.swing.JButton();
+        btnConsultar = new javax.swing.JButton();
         btnVerificarDatos = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(0, 0, 0));
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        btnGuardar.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        btnGuardar.setText("Guardar");
-        btnGuardar.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnGuardarActionPerformed(evt);
-            }
-        });
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(255, 0, 0));
         jLabel1.setText("Nombre:");
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 30, -1, -1));
+        jPanel1.add(txtNombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 10, 227, 48));
 
+        jLabel2.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
         jLabel2.setForeground(new java.awt.Color(255, 0, 0));
         jLabel2.setText("Teléfono:");
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 110, -1, -1));
+        jPanel1.add(txtTelefono, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 90, 227, 48));
 
+        jLabel3.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
         jLabel3.setForeground(new java.awt.Color(255, 0, 0));
         jLabel3.setText("Gmail:");
+        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 190, -1, -1));
+        jPanel1.add(txtGmail, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 170, 227, 48));
 
         btnLimpiar.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         btnLimpiar.setText("Limpiar");
@@ -62,6 +72,42 @@ public class Registro extends javax.swing.JFrame {
                 btnLimpiarActionPerformed(evt);
             }
         });
+        jPanel1.add(btnLimpiar, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 90, 140, 40));
+
+        btnGuardar.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        btnGuardar.setText("Guardar");
+        btnGuardar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnGuardarActionPerformed(evt);
+            }
+        });
+        jPanel1.add(btnGuardar, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 20, 140, 40));
+
+        jScrollPane1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+
+        jtxtVerDatos.setColumns(20);
+        jtxtVerDatos.setRows(5);
+        jScrollPane1.setViewportView(jtxtVerDatos);
+
+        jPanel1.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 230, 630, 204));
+
+        btnEliminar.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        btnEliminar.setText("Eliminar");
+        btnEliminar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnEliminarActionPerformed(evt);
+            }
+        });
+        jPanel1.add(btnEliminar, new org.netbeans.lib.awtextra.AbsoluteConstraints(510, 20, 140, 40));
+
+        btnModificar.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        btnModificar.setText("Modificar");
+        btnModificar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnModificarActionPerformed(evt);
+            }
+        });
+        jPanel1.add(btnModificar, new org.netbeans.lib.awtextra.AbsoluteConstraints(510, 90, 140, 40));
 
         btnConsultar.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         btnConsultar.setText("Consultar");
@@ -70,10 +116,7 @@ public class Registro extends javax.swing.JFrame {
                 btnConsultarActionPerformed(evt);
             }
         });
-
-        jtxtVerDatos.setColumns(20);
-        jtxtVerDatos.setRows(5);
-        jScrollPane1.setViewportView(jtxtVerDatos);
+        jPanel1.add(btnConsultar, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 160, 140, 40));
 
         btnVerificarDatos.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         btnVerificarDatos.setText("VerificarDatos");
@@ -82,67 +125,9 @@ public class Registro extends javax.swing.JFrame {
                 btnVerificarDatosActionPerformed(evt);
             }
         });
+        jPanel1.add(btnVerificarDatos, new org.netbeans.lib.awtextra.AbsoluteConstraints(510, 160, 140, 40));
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(10, 10, 10)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 730, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel1)
-                            .addComponent(jLabel2)
-                            .addComponent(jLabel3))
-                        .addGap(21, 21, 21)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(txtNombre, javax.swing.GroupLayout.PREFERRED_SIZE, 227, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(txtTelefono, javax.swing.GroupLayout.PREFERRED_SIZE, 227, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(txtGmail, javax.swing.GroupLayout.PREFERRED_SIZE, 227, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(83, 83, 83)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(btnGuardar, javax.swing.GroupLayout.PREFERRED_SIZE, 173, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(btnLimpiar, javax.swing.GroupLayout.PREFERRED_SIZE, 173, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(17, 17, 17)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(btnConsultar, javax.swing.GroupLayout.PREFERRED_SIZE, 173, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(btnVerificarDatos, javax.swing.GroupLayout.PREFERRED_SIZE, 173, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                .addContainerGap(21, Short.MAX_VALUE))
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(30, 30, 30)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(txtNombre, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(22, 22, 22)
-                        .addComponent(txtTelefono, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(32, 32, 32)
-                        .addComponent(txtGmail, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(10, 10, 10)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(jLabel1)
-                                .addGap(64, 64, 64)
-                                .addComponent(jLabel2)
-                                .addGap(64, 64, 64)
-                                .addComponent(jLabel3))
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(btnGuardar, javax.swing.GroupLayout.PREFERRED_SIZE, 66, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(24, 24, 24)
-                                .addComponent(btnLimpiar, javax.swing.GroupLayout.PREFERRED_SIZE, 66, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(btnConsultar, javax.swing.GroupLayout.PREFERRED_SIZE, 66, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(24, 24, 24)
-                                .addComponent(btnVerificarDatos, javax.swing.GroupLayout.PREFERRED_SIZE, 66, javax.swing.GroupLayout.PREFERRED_SIZE)))))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 40, Short.MAX_VALUE)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(17, 17, 17))
-        );
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 670, 450));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -160,11 +145,12 @@ public class Registro extends javax.swing.JFrame {
             insertar.setString(2, nombre);
             insertar.setString(3, telefono);
             insertar.setString(4, gmail);
-            insertar.executeUpdate();
+            insertar.executeUpdate(); // hacemos la petionc
             JOptionPane.showMessageDialog(null, "Datos guardados correctamente");
+            actualizarTable();
             conectar.cerrarConexion();
 
-        } catch (Exception e) {
+        } catch (SQLException e) {
             System.err.println("Error: " + e);
         }
 
@@ -177,12 +163,15 @@ public class Registro extends javax.swing.JFrame {
     }//GEN-LAST:event_btnLimpiarActionPerformed
 
     private void btnConsultarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnConsultarActionPerformed
+        actualizarTable();
+    }//GEN-LAST:event_btnConsultarActionPerformed
+    private void actualizarTable() {
         jtxtVerDatos.setText("");
         try {
             Connection conexion = this.conectar.conectar();
             PreparedStatement selecionar = conexion.prepareStatement("SELECT * FROM personas");
-            ResultSet consulta = selecionar.executeQuery();
-            while(consulta.next()){
+            ResultSet consulta = selecionar.executeQuery(); // realiza consultas
+            while (consulta.next()) { // si la variable tiene un siguiente 
                 jtxtVerDatos.append(consulta.getString(1));
                 jtxtVerDatos.append("  ");
                 jtxtVerDatos.append(consulta.getString(2));
@@ -193,13 +182,11 @@ public class Registro extends javax.swing.JFrame {
                 jtxtVerDatos.append("\n");
             }
             conectar.cerrarConexion();
-            
+
         } catch (Exception e) {
             System.err.println("Error: " + e);
         }
-        
-    }//GEN-LAST:event_btnConsultarActionPerformed
-
+    }
     private void btnVerificarDatosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVerificarDatosActionPerformed
         try {
             Connection conexion = this.conectar.conectar();
@@ -212,32 +199,83 @@ public class Registro extends javax.swing.JFrame {
                 JOptionPane.showMessageDialog(null, "La base de datos no tiene datos");
 
             }
-            
+
             conectar.cerrarConexion();
 
         } catch (Exception e) {
             System.err.println("Error: " + e);
         }
 
-       
+
     }//GEN-LAST:event_btnVerificarDatosActionPerformed
 
-    public static void main(String args[]) {
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new Registro().setVisible(true);
+    private void btnEliminarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEliminarActionPerformed
+        try {
+            String id = JOptionPane.showInputDialog("Digite el id de la persona que desea eliminar");
+            Connection conexion = conectar.conectar();
+            PreparedStatement eliminar = conexion.prepareStatement("DELETE FROM personas WHERE id = ?");
+            eliminar.setString(1, id);
+            eliminar.executeUpdate(); // ejecutamos la peticion
+            int elimino = eliminar.getUpdateCount();
+            if (elimino != 0) {
+                JOptionPane.showMessageDialog(null, "Persona eliminada");
+
+            } else {
+                JOptionPane.showMessageDialog(null, "Persona no encontrada");
+
             }
-        });
-    }
+
+            actualizarTable();
+            conectar.cerrarConexion();
+
+        } catch (SQLException e) {
+            System.err.println(e);
+        }
+
+
+    }//GEN-LAST:event_btnEliminarActionPerformed
+
+    private void btnModificarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnModificarActionPerformed
+
+        try {
+
+            Connection conexion = conectar.conectar();
+            String id = JOptionPane.showInputDialog("Digite el id de la persona que desea modificar");
+
+            PreparedStatement modificar = conexion.prepareStatement("UPDATE  personas SET nombre = ?, telefono = ?, gmail = ?"
+                    + " WHERE id = " + id);
+            modificar.setString(1, txtNombre.getText());
+            modificar.setString(2, txtTelefono.getText());
+            modificar.setString(3, txtGmail.getText());
+            modificar.executeUpdate();
+            int encontro = modificar.getUpdateCount();
+            if (encontro != 0) {
+                JOptionPane.showMessageDialog(null, "Persona modificada");
+            } else {
+                JOptionPane.showMessageDialog(null, "Persona no encontrada");
+
+            }
+            actualizarTable();
+            conectar.cerrarConexion();
+
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+
+    }//GEN-LAST:event_btnModificarActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnConsultar;
+    private javax.swing.JButton btnEliminar;
     private javax.swing.JButton btnGuardar;
     private javax.swing.JButton btnLimpiar;
+    private javax.swing.JButton btnModificar;
     private javax.swing.JButton btnVerificarDatos;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextArea jtxtVerDatos;
     private javax.swing.JTextField txtGmail;
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,14 @@
 package main;
 
+import Vistas.Registro;
+
 
 public class Main {
 
     
     public static void main(String[] args) {
+        Registro re = new Registro();
+        re.setVisible(true);
         
     }
     
